bbc249e33108f78821f803eee6700029ca87140c
ShivaRK <shivarkcodes@gmail.com>
2025-03-12 15:20:38 +0530

    added cert images

--- app/routes/pages.about-us.jsx
+++ app/routes/pages.about-us.jsx
@@ -178,22 +178,22 @@ export default function AboutUs() {
 
             <div className='my-20'>
                 <div className='w-full md:flex hidden flex-wrap justify-between items-center px-5'>
-                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }} className='h-28' src="/home/certs/cert1.png" alt="" />
-                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.2 }} className='h-28' src="/home/certs/cert2.png" alt="" />
-                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.3 }} className='h-28' src="/home/certs/cert3.png" alt="" />
-                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.4 }} className='h-28' src="/home/certs/cert4.png" alt="" />
-                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.5 }} className='h-28' src="/home/certs/cert5.png" alt="" />
-                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.6 }} className='h-28' src="/home/certs/cert6.png" alt="" />
-                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.7 }} className='h-28' src="/home/certs/cert7.png" alt="" />
+                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.1 }} className='h-28' src="/home/certs/1.svg" alt="" />
+                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.2 }} className='h-28' src="/home/certs/2.svg" alt="" />
+                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.3 }} className='h-28' src="/home/certs/3.svg" alt="" />
+                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.4 }} className='h-28' src="/home/certs/4.svg" alt="" />
+                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.5 }} className='h-28' src="/home/certs/5.svg" alt="" />
+                    <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.6 }} className='h-28' src="/home/certs/6.svg" alt="" />
+                    {/* <motion.img initial={{ opacity: 0, y: 70 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.3, delay: 0.7 }} className='h-28' src="/home/certs/7.svg" alt="" /> */}
                 </div>
                 <div className='w-full md:hidden overflow-y-scroll md:flex-wrap flex justify-between items-center px-5' style={{ scrollbarWidth: "none" }}>
-                    <img className='h-28' src="/home/certs/cert1.png" alt="" />
-                    <img className='h-28' src="/home/certs/cert2.png" alt="" />
-                    <img className='h-28' src="/home/certs/cert3.png" alt="" />
-                    <img className='h-28' src="/home/certs/cert4.png" alt="" />
-                    <img className='h-28' src="/home/certs/cert5.png" alt="" />
-                    <img className='h-28' src="/home/certs/cert6.png" alt="" />
-                    <img className='h-28' src="/home/certs/cert7.png" alt="" />
+                    <img className='h-28' src="/home/certs/1.svg" alt="" />
+                    <img className='h-28' src="/home/certs/2.svg" alt="" />
+                    <img className='h-28' src="/home/certs/3.svg" alt="" />
+                    <img className='h-28' src="/home/certs/4.svg" alt="" />
+                    <img className='h-28' src="/home/certs/5.svg" alt="" />
+                    <img className='h-28' src="/home/certs/6.svg" alt="" />
+                    {/* <img className='h-28' src="/home/certs/7.svg" alt="" /> */}
                 </div>
             </div>
 
